b2c04b237d68513537401f4d11e1f724e1a55f65
sofiegillespie <sofie@monkeysleeps.com>
2021-09-16 21:10:10 +0100

    added api call for forecast

--- Week5.js
+++ Week5.js
@@ -132,7 +132,8 @@ function getCurrentLocation(event) {
 let currentLocationButton = document.querySelector("#location-button");
 currentLocationButton.addEventListener("click", getCurrentLocation);
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let days = ["Fri", "Sat", "Sun", "Mon", "Tues"];
@@ -159,4 +160,9 @@ function displayForecast() {
   console.log(forecastHTML);
 }
 
-displayForecast();
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "5f472f11cd19ee1ec953a54c96fd4be0fcb4";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
